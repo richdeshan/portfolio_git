056121dd9dde2c67676618e9b357a077c4fdb77c
starspen <richdeshan@gmail.com>
2024-09-07 12:37:49 +0700

    Menambahkan file portfolio

---
+++ react-portfolio/src/components/About/About.jsx
@@ -0,0 +1,34 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import styles from './About.module.css'
+import { getImageUrl } from '../../utils'
+
+export const About = () => {
+  return (
+    <section className={styles.container} id='about'>
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img 
+          className={styles.aboutImage} src={getImageUrl("about/aboutImage.png")} alt='me sitting with a laptop'>
+        </img>
+      
+    <ul className={styles.aboutItems}>
+      <li className={styles.aboutItem}>
+        <img src={getImageUrl("about/cursorIcon.png")} alt='cursor icon'></img>
+        <div className={styles.aboutItemText}>
+          <h3>Front End Developer</h3>
+          <p>I'm a frontend developer with experience in building responsive and optimized sites</p>
+        </div>
+      </li>
+      <li className={styles.aboutItem}>
+        <img className={styles.machineLearning} src={getImageUrl("about/ML.png")} alt='cursor icon'></img>
+        <div className={styles.aboutItemText}>
+          <h3>Data Science and Machine Learning/Deep Learning</h3>
+          <p>I have experience in Data Science and Machine Learning, working with tools like Python, Pandas, NumPy, TensorFlow, and scikit-learn. I’ve built predictive models for real-world applications, focusing on data-driven solutions to improve decision-making and optimize performance.</p>
+        </div>
+      </li>
+    </ul>
+    </div>
+    </section>
+  )
+}
---
+++ react-portfolio/src/components/Contact/Contact.jsx
@@ -0,0 +1,33 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:richdeshan@gmail.com">richdeshan@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/richdjuardi">linkedin.com/richdjuardi</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://www.github.com/myname">github.com/myname</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
