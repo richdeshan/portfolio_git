bcac3bd8bce4d8042238bb344c9ced733e6b4bb9
starspen <71621186+starspen@users.noreply.github.com>
2024-09-10 16:59:46 +0700

    Update Hero.jsx

--- react-portfolio/src/components/Hero/Hero.jsx
+++ react-portfolio/src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ export const Hero = () => {
   return( 
   <section className={styles.container}>
     <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I'm Rich</h1>
+        <h1 className={styles.title}>Hi, I'm Rich Deshan Djuardi</h1>
         <p className={styles.description}>I'm a fresh graduate with a passion for front-end web development. With a strong foundation in HTML, CSS, JavaScript, and frameworks like Bootstrap and React, I am excited to build engaging, responsive, and user-friendly websites. I am always eager to learn new technologies and continuously improve my skills. Let's collaborate to create amazing digital experiences!</p>
         <a href="mailto:richdeshan@gmail.com" className={styles.contactBtn}>Contact Me</a>
     </div>
