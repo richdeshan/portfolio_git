77c9df5630366cd7268ecdb10fd78759246b3616
starspen <richdeshan@gmail.com>
2024-09-10 16:39:40 +0700

    Update About and Hero components, add new images

--- react-portfolio/src/components/About/About.jsx
+++ react-portfolio/src/components/About/About.jsx
@@ -9,7 +9,7 @@ export const About = () => {
       <h2 className={styles.title}>About</h2>
       <div className={styles.content}>
         <img 
-          className={styles.aboutImage} src={getImageUrl("about/aboutImage.png")} alt='me sitting with a laptop'>
+          className={styles.aboutImage} src={getImageUrl("about/fotoAbout.jpg")} alt='me sitting with a laptop'>
         </img>
       
     <ul className={styles.aboutItems}>
--- react-portfolio/src/components/Hero/Hero.jsx
+++ react-portfolio/src/components/Hero/Hero.jsx
@@ -11,7 +11,7 @@ export const Hero = () => {
         <p className={styles.description}>I'm a fresh graduate with a passion for front-end web development. With a strong foundation in HTML, CSS, JavaScript, and frameworks like Bootstrap and React, I am excited to build engaging, responsive, and user-friendly websites. I am always eager to learn new technologies and continuously improve my skills. Let's collaborate to create amazing digital experiences!</p>
         <a href="mailto:richdeshan@gmail.com" className={styles.contactBtn}>Contact Me</a>
     </div>
-   <img src={getImageUrl('hero/heroImage.png')} alt="hero" className={styles.heroImg}/>
+   <img src={getImageUrl('hero/Foto.png')} alt="hero" className={styles.heroImg}/>
   <div className={styles.topBlur}/>
   <div className={styles.bottomBlur}></div>
   </section>
